d2a13b7bfacefd3bc378cba9daa3cabd67ed3d03
apro <oligoing@gmail.com>
2021-09-03 11:21:05 +0900

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    id(ScriptPlugins.infrastructure)
+}
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+
+    dependencies {
+        classpath (BuildPlugins.androidGradlePlugin)
+        classpath (BuildPlugins.kotlinGradlePlugin)
+        classpath (BuildPlugins.hiltGradlePlugin)
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
